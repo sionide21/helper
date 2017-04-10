7c2a0e75571cf0c384aa8cf6f5c85c59e6dae5d9
Ben Olive <sionide21@gmail.com>
2017-04-09 22:35:00 -0400

    Better error message

--- apps/helper_core/lib/helper_core/commands/not_found.ex
+++ apps/helper_core/lib/helper_core/commands/not_found.ex
@@ -3,7 +3,7 @@ defmodule HelperCore.Commands.NotFound do
 
   def handle_command(command) do
     command
-    |> execute(:error, "#{command.name}: command not found")
+    |> execute(:error, "H.E.L.P.eR.: #{command.name}: command not found")
     |> execute(:exit, 1)
 
     command
